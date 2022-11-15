4c7b9ca4310bb5cda727ade7e95f781b85bb2d9b
Senkidev <dante.vilchesc@gmail.com>
2022-11-15 13:31:26 -0300

    Vue index, upcoming and past events

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,126 +1,58 @@
-let cards = document.getElementById("cards");
-let containerCheck = document.getElementById("containerCheck");
-let form = document.getElementById("formListen");
-let info;
-let data;
-//Crear checkbox
-let fn = (category) => category.category;
-let eventsWithCategory;
-let categories;
-let categoriesNoRepeat;
-let arrayCategoriesNoRepeat;
-fetch("https://amazing-events.herokuapp.com/api/events")
-	.then((response) => response.json())
-	.then((json) => {
-		info = json;
-		data = info.events;
+const { createApp } = Vue;
 
-		execute();
-	})
-	.catch((error) => console.log(error));
+const app = createApp({
+	data() {
+		return {
+			events: [],
+			filteredEvents: [],
+			categories: [],
+			inputSearch: "",
+			checked: [],
+			dataEvents: [],
+		};
+	},
 
-function execute() {
-	eventsWithCategory = data.filter(fn);
-	categories = eventsWithCategory.map(fn);
-	categoriesNoRepeat = new Set(categories);
-	arrayCategoriesNoRepeat = Array.from(categoriesNoRepeat);
-	createCheckbox(arrayCategoriesNoRepeat, containerCheck);
-	renderCards(eventsWithCategory, cards);
-	containerCheck.addEventListener("change", filterSearch);
-	form.addEventListener("submit", (e) => {
-		e.preventDefault();
-	});
-	searchBar.addEventListener("keyup", filterSearch);
-}
+	created() {
+		fetch("https://amazing-events.herokuapp.com/api/events")
+			.then((response) => response.json())
+			.then((json) => {
+				this.info = json;
+				this.dataEvents = this.info.events;
 
-function createCheckbox(array, container) {
-	let aux = "";
-	array.forEach(
-		(value) =>
-			(aux += ` <label class="btn btn-dark active "><input type="checkbox"
-									value="${value}"								
-								/>${value}
-	 </label>`)
-	);
+				this.filteredEvents = this.dataEvents;
+				this.getCategories();
+			})
+			.catch((error) => console.log(error));
+	},
+	methods: {
+		getCategories() {
+			let fn = (events) => events.category;
+			this.categories = [...new Set(this.dataEvents.filter(fn).map(fn))];
+		},
+		find() {
+			this.filteredEvents = this.dataEvents.filter((events) =>
+				events.name
+					.toLowerCase()
+					.trim()
+					.includes(this.inputSearch.toLowerCase().trim())
+			);
+		},
+	},
 
-	container.innerHTML = aux;
-}
+	computed: {
+		filter() {
+			const eventsFilteredByCategory = this.dataEvents.filter(
+				(events) =>
+					this.checked.includes(events.category) || this.checked.length === 0
+			);
+			this.filteredEvents = eventsFilteredByCategory.filter((events) =>
+				events.name
+					.toLowerCase()
+					.trim()
+					.includes(this.inputSearch.toLowerCase().trim())
+			);
+		},
+	},
+});
 
-//Crear cards
-
-function createCard(evento) {
-	let div = document.createElement("div");
-	div.classList.add(`card`);
-	div.innerHTML += `<img src="${evento.image}"
-						class="card-img-top"
-						alt="Image of ${evento.name}"/>
-					<div class="card-body d-flex flex-column align-items-center">
-						<h3 class="card-title">${evento.name}</h3>
-						<p class="card-text">
-							${evento.description}
-						</p>
-						<h3 class="mt-auto">
-							$ ${evento.price}
-						</h3>
-						<a
-							href="./details.html?id=${evento._id}"
-							class="btn btn-primary align-self-stretch "
-							>More info...</a
-						>
-					</div>`;
-	return div;
-}
-
-function renderCards(events, container) {
-	container.innerHTML = "";
-	if (events.length == 0) {
-		cards.innerHTML = `<h2 class="mt-5 text-light bg-dark p-3">There are no events to show</h2>`;
-	} else {
-		let fragment = document.createDocumentFragment();
-		events.forEach((evento) => fragment.appendChild(createCard(evento)));
-
-		container.appendChild(fragment);
-	}
-}
-
-//filtro checkbox
-
-function filterCheck() {
-	const checked = Array.from(
-		document.querySelectorAll('input[type="checkbox"]:checked')
-	).map((input) => input.value);
-
-	const filteredEvents = eventFilterByCategory(eventsWithCategory, checked);
-
-	filteredEvents.length !== 0
-		? renderCards(filteredEvents, cards)
-		: (cards.innerHTML = "<h2>There are no events to show</h2>");
-
-	return filteredEvents;
-}
-
-function eventFilterByCategory(events, categoriesSelected) {
-	let fn = (evento) =>
-		categoriesSelected.includes(evento.category) ||
-		categoriesSelected.length === 0;
-	let filtered = events.filter(fn);
-	return filtered;
-}
-
-// filtro Searchbar
-
-let searchBar = document.querySelector("input[type=search]");
-
-function filterSearch() {
-	let filterInput = searchBar.value.toLowerCase().trim();
-	let aux = [];
-
-	filterCheck().forEach((element) => {
-		if (element.name.toLocaleLowerCase().includes(filterInput)) {
-			aux.push(element);
-		} else {
-		}
-	});
-
-	renderCards(aux, cards);
-}
+app.mount("#app");
