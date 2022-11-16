21609810076627d06da7edddece811f3476a160f
Senkidev <dante.vilchesc@gmail.com>
2022-11-16 12:37:56 -0300

    Stats y details con VUE

--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -1,42 +1,29 @@
-const queryString = location.search;
+const { createApp } = Vue;
 
-let data;
-const params = new URLSearchParams(queryString);
-const id = params.get("id");
-let evento;
+const app = createApp({
+	data() {
+		return {
+			dataEvents: [],
+			evento: [],
+		};
+	},
 
-fetch("https://amazing-events.herokuapp.com/api/events")
-	.then((response) => response.json())
-	.then((json) => {
-		info = json;
-		data = info.events;
-		evento = data.find((eventos) => eventos._id == id);
-		createCard();
-	})
-	.catch((error) => console.log(error));
-let sectionDetail = document.getElementById("sectionDetail");
+	created() {
+		fetch("https://amazing-events.herokuapp.com/api/events")
+			.then((response) => response.json())
+			.then((json) => {
+				this.info = json;
+				this.dataEvents = this.info.events;
+				queryString = location.search;
+				params = new URLSearchParams(queryString);
+				this.id = params.get("id");
+				this.evento = this.dataEvents.find((eventos) => eventos._id == this.id);
+			})
+			.catch((error) => console.log(error));
+	},
+	methods: {},
 
-function createCard() {
-	sectionDetail.innerHTML = `<div class="w-50 h-75 p-3 border border-dark d-flex justify-content-center align-items-center detailImg gap-2">
-		<img src="${evento.image}" alt="${evento.name}" />
-	</div>
-	<div class="d-flex flex-column justify-content-center align-items-center w-50 h-75 p-3 border border-dark detailP">
-		
-    <h3>${evento.name}</h3>
+	computed: {},
+});
 
-		<div>
-			<p>Category: ${evento.category}</p>
-			
-			<p> Date: ${evento.date}</p>
-            <p>Capacity: ${evento.capacity}</p>
-			<p>${
-				evento.estimate
-					? "Estimate: " + evento.estimate
-					: "Asisstance: " + evento.assistance
-			}</p>
-			<p>${evento.description}</p>
-			
-			<h3>Price: $${evento.price}</h3>
-		</div>
-	</div> `;
-}
+app.mount("#app");
--- assets/javascript/stats.js
+++ assets/javascript/stats.js
@@ -1,11 +1,89 @@
-document.getElementById("tabla").style.overflow = "scroll";
-fetch("https://amazing-events.herokuapp.com/api/events")
-	.then((response) => response.json())
-	.then((json) => {
-		info = json;
-		data = info.events;
+const { createApp } = Vue;
 
-		data.forEach((event) => {
+const app = createApp({
+	data() {
+		return {
+			dataEvents: [],
+			objCategoriesUpcoming: [],
+			objCategoriesPast: [],
+			upcomingCategoriesNoRepeat: [],
+			largerCapacity: [],
+			lowestPercentage: [],
+			highestPercentage: [],
+			eventsWithCategoryPast: [],
+			info: {},
+			pastEvents: [],
+			upcomingEvents: [],
+		};
+	},
+
+	created() {
+		fetch("https://amazing-events.herokuapp.com/api/events")
+			.then((response) => response.json())
+			.then((json) => {
+				this.info = json;
+				this.dataEvents = this.info.events;
+
+				this.dataEvents.forEach((event) => this.addProperty(event));
+				this.upcomingPastFilter();
+				this.pastEvents.sort((a, b) => b.percentage - a.percentage);
+				this.largerCapacity = this.dataEvents
+					.sort((a, b) => b.capacity - a.capacity)
+					.slice(0, 1)[0].name;
+
+				this.lowestPercentage = this.pastEvents.slice(-1)[0].name;
+				this.highestPercentage = this.pastEvents.slice(0, 1)[0].name;
+
+				let fn = (category) => category.category;
+				this.eventsWithCategoryPast = this.pastEvents.filter(fn);
+				this.pastCategoriesNoRepeat = Array.from(
+					new Set(this.eventsWithCategoryPast.map(fn))
+				);
+
+				this.eventsWithCategoryUpcoming = this.upcomingEvents.filter(fn);
+				this.upcomingCategoriesNoRepeat = Array.from(
+					new Set(this.eventsWithCategoryUpcoming.map(fn))
+				);
+
+				this.createObj(
+					this.upcomingCategoriesNoRepeat,
+					this.upcomingEvents,
+					this.objCategoriesUpcoming
+				);
+
+				this.createObj(
+					this.pastCategoriesNoRepeat,
+					this.pastEvents,
+					this.objCategoriesPast
+				);
+			})
+			.catch((error) => console.log(error));
+	},
+	methods: {
+		createObj(arrayNoRepeat, arrayUpcomingOrPast, objName) {
+			arrayNoRepeat.sort().forEach((category) => {
+				let obj = {
+					name: "",
+					revenue: 0,
+					percentage: 0,
+				};
+
+				obj.name = category;
+				obj.revenue = arrayUpcomingOrPast
+					.filter((events) => events.category == category)
+					.map((events) => events.revenue)
+					.reduce((a, b) => a + b, 0);
+
+				obj.percentage =
+					arrayUpcomingOrPast
+						.filter((events) => events.category == category)
+						.map((events) => events.percentage)
+						.reduce((a, b) => a + b, 0) /
+					arrayUpcomingOrPast.filter((events) => events.category == category).length;
+				objName.push(obj);
+			});
+		},
+		addProperty(event) {
 			event.percentage = parseInt(
 				event.assistance
 					? ((event.assistance * 100) / event.capacity).toFixed(2)
@@ -17,87 +95,19 @@ fetch("https://amazing-events.herokuapp.com/api/events")
 					? event.assistance * event.price
 					: event.estimate * event.price
 			);
-		});
-		execute();
-	})
-	.catch((error) => console.log(error));
-let data;
-let info;
-let fn = (category) => category.category;
-let highest = document.getElementById("highest");
-let lowest = document.getElementById("lowest");
-let larger = document.getElementById("larger");
-let upcomingTable = document.getElementById("upcomingTable");
-let pastTable = document.getElementById("pastTable");
-let pastEvents;
-let upcomingEvents;
-let objCategoriesUpcoming = [];
-let objCategoriesPast = [];
-let eventsWithCategoryPast;
-let pastCategoriesNoRepeat;
-let eventsWithCategoryUpcoming;
-let upcomingCategoriesNoRepeat;
-function execute() {
-	console.log(data);
-	pastEvents = data.filter((event) => event.date < info.currentDate);
+		},
+		upcomingPastFilter() {
+			this.pastEvents = this.dataEvents.filter(
+				(event) => event.date < this.info.currentDate
+			);
 
-	upcomingEvents = data.filter((event) => event.date > info.currentDate);
-	pastEvents.sort((a, b) => b.percentage - a.percentage);
-	largerCapacity = data.sort((a, b) => b.capacity - a.capacity).slice(0, 1);
-	lowestPercentage = pastEvents.slice(-1);
-	highestPercentage = pastEvents.slice(0, 1);
+			this.upcomingEvents = this.dataEvents.filter(
+				(event) => event.date > this.info.currentDate
+			);
+		},
+	},
 
-	//Create highest %/ lowest %/ larger capacity  events
-	highest.innerHTML = highestPercentage[0].name;
-	lowest.innerHTML = lowestPercentage[0].name;
-	larger.innerHTML = largerCapacity[0].name;
+	computed: {},
+});
 
-	eventsWithCategoryPast = pastEvents.filter(fn);
-	pastCategoriesNoRepeat = Array.from(new Set(eventsWithCategoryPast.map(fn)));
-
-	eventsWithCategoryUpcoming = upcomingEvents.filter(fn);
-	upcomingCategoriesNoRepeat = Array.from(
-		new Set(eventsWithCategoryUpcoming.map(fn))
-	);
-
-	function createObj(arrayNoRepeat, arrayUpcomingOrPast, objName) {
-		arrayNoRepeat.sort().forEach((category) => {
-			let obj = {
-				name: "",
-				revenue: 0,
-				percentage: 0,
-			};
-
-			obj.name = category;
-			obj.revenue = arrayUpcomingOrPast
-				.filter((events) => events.category == category)
-				.map((events) => events.revenue)
-				.reduce((a, b) => a + b, 0);
-
-			obj.percentage =
-				arrayUpcomingOrPast
-					.filter((events) => events.category == category)
-					.map((events) => events.percentage)
-					.reduce((a, b) => a + b, 0) /
-				arrayUpcomingOrPast.filter((events) => events.category == category).length;
-			objName.push(obj);
-		});
-	}
-	createObj(upcomingCategoriesNoRepeat, upcomingEvents, objCategoriesUpcoming);
-	createObj(pastCategoriesNoRepeat, pastEvents, objCategoriesPast);
-
-	function createTableRow(objUpcomingOrPast, container) {
-		objUpcomingOrPast.forEach((event) => {
-			container.innerHTML += `
-		<tr>
-			<td>${event.name}</td>
-			<td>$${event.revenue}</td>
-			<td>${event.percentage.toFixed(2)}%</td>
-		</tr>
-		
-		`;
-		});
-	}
-	createTableRow(objCategoriesUpcoming, upcomingTable);
-	createTableRow(objCategoriesPast, pastTable);
-}
+app.mount("#app");
